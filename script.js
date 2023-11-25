0ad2447265acb84799871d27d9c27cfa1b0ad482
Francisco Díaz <fs.diazr@gmail.com>
2023-11-25 15:29:04 -0300

    Pagina de practica con modal

---
+++ script.js
@@ -0,0 +1,19 @@
+function mostrarModal() {
+  let input = document.querySelector("input");
+
+  document.querySelector(
+    ".modal-body p"
+  ).innerHTML = `Estás buscando: ${input.value}`;
+
+  //Despliega modal
+  document.querySelector(".modal").classList.remove("display-none");
+
+  //Capa atenuante
+  document.querySelector("body").classList.add("modal-on");
+}
+
+function cerrarModal() {
+  document.querySelector(".modal").classList.add("display-none");
+
+  document.querySelector("body").classList.remove("modal-on");
+}
